799e61f776cb22dfbfd97738bc186c9edff60c03
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-09-21 17:26:16 +0530

    added models for chat, user and message

---
+++ backend/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePhoto: {
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+const User = mongoose.model("User", userModel);
+module.exports = User;
