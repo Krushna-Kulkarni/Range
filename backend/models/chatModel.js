5e896b6d2ca0773adb2305f5b211297d01619a1f
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-09-22 18:15:42 +0530

    added search user and Chat Api

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const chatModel = mongoose.Schema(
+const chatSchema = mongoose.Schema(
   {
     chatName: {
       type: String,
@@ -30,6 +30,6 @@ const chatModel = mongoose.Schema(
   }
 );
 
-const Chat = mongoose.model("Chat", chatModel);
+const Chat = mongoose.model("Chat", chatSchema);
 
 module.exports = Chat;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 // one way of writing route
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, allUsers);
+// router.route("/").get(allUsers);
 
 // //another way of writing route
 router.post("/login", authUser);
